e83f57f03c131ebfc2cc1f108df49fe0cc35db85
Nina Sandejas <nina.sandejas@student.ateneo.edu>
2024-03-04 23:33:52 +0800

    Edited views and URLs

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,13 +1,10 @@
 # <appname>/urls.py
 from django.urls import path
-from .views import index
-from .views import recipe1, recipe2, recipes_list
+from .views import RecipeList, Recipe
 
 urlpatterns = [
-path('', index, name='index'),
-path('recipes/list', recipes_list, name='recipes_list'),
-path('recipe/1', recipe1, name='recipes_list'),
-path('recipe/2', recipe2, name='recipes_list'),
+    path('recipes/list', RecipeList.as_view(), name='recipe_site'),
+    path('recipe/<int:pk>/', Recipe.as_view(), name='recipe'),
 ]
 
 app_name = "ledger"
