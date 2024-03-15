f0a9e68d3dc99e1a5e4db57b6e6584e6c5a2554b
Nina Sandejas <nina.sandejas@student.ateneo.edu>
2024-03-15 20:39:38 +0800

    Added Profile model to admin

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,16 +1,36 @@
 from django.contrib import admin
 
-from .models import Recipe, RecipeIngredient, Ingredient
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
+from .models import Recipe, RecipeIngredient, Ingredient, Profile
+
+
+
+
+class ProfileInLine(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    
+
+class UserAdmin(BaseUserAdmin):
+    inlines = [ProfileInLine,]
+
 
 class RecipeInline(admin.StackedInline):
     model = RecipeIngredient
+
 
 class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
     inlines = [RecipeInline]
 
+
 class IngredientAdmin(admin.ModelAdmin):
     model = Ingredient
+    
 
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Ingredient, IngredientAdmin)
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-
 from .models import Recipe
+
+
 
 class RecipeList(ListView):
     model = Recipe 
