9e8163fd71bde8c00f3da17b39c69da36612990b
Nina Sandejas <nina.sandejas@student.ateneo.edu>
2024-03-15 19:54:46 +0800

    Modified the Recipe model

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,8 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
-from django.contrib.auth.models import User
 
 
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.TextField(min_length=255)
+
+    def __str__(self):
+        return '{}'.format(self.name)
 
 
 class Ingredient(models.Model):
@@ -17,6 +26,15 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
+    author = models.ForeignKey(
+        Profile,
+        null=True,
+        default=None,
+        on_delete=models.CASCADE,
+        related_name="users"
+    )
+    created_on = models.DateTimeField(auto_now_add=True, null=True)
+    updated_on = models.DateTimeField(auto_now=True, null=True)
 
     def __str__(self):
         return '{}'.format(self.name)
@@ -38,12 +56,3 @@ class RecipeIngredient(models.Model):
         on_delete=models.CASCADE,
         related_name = 'ingredients'
     )
-
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50)
-    bio = models.TextField(min_length=255)
-
-    def __str__(self):
-        return '{}'.format(self.name)
