0c9259d159d23d9ea3a87df9ccb4e66b08f76316
Nina Sandejas <nina.sandejas@student.ateneo.edu>
2024-03-15 23:29:09 +0800

    Minor editing

--- recipebook/recipebook/urls.py
+++ recipebook/recipebook/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ledger.urls', namespace="ledger")),
+    path('/accounts', include('django.contrib.auth.urls')),
 ]
